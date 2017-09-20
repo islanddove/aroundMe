0311ea9fa205c61ecf8d8724fa4b48fc28761d8a
Daniel Emanuel Puleio <depuleio@buffalo.edu>
2017-09-20 14:45:04 -0400

    got AJAX working

--- server/app/views/rest_views.py
+++ server/app/views/rest_views.py
@@ -4,16 +4,17 @@ from __future__ import unicode_literals
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Event 
+from ..models import Event 
+import json
 
-# rest (AJAX HTTPS requests)
+# rest (AJAX HTTPS requests) for AJAX only
 @csrf_exempt
 def createEvent(request):
-
-	print "GOT HERE"
-	title = request.POST["title"]
-	desc = request.POST["desc"]
-	rsvp = request.POST["rsvp"]
+	print "type " + str(type(request.body))
+	data = json.loads(request.body)
+	title = data["title"]
+	desc = data["desc"]
+	rsvp = data["rsvp"]
 
 	newEvent = Event(event_title= title, description = desc, rsvp = rsvp)
 	newEvent.save()
