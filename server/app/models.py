bb3e8266f53f8b863b8d980cc2fa56d5b67a2187
Daniel Emanuel Puleio <depuleio@buffalo.edu>
2017-09-21 23:11:13 -0400

    Added admin page

--- server/app/models.py
+++ server/app/models.py
@@ -8,6 +8,17 @@ from django.utils import timezone
 
 
 class Event(models.Model):	
+
+    
+    event_title = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    pub_date = models.DateTimeField(default=timezone.now)
+    rsvp = models.BooleanField(default=False)
+
+    """
+
+    private = models.BooleanField(default=False)
+
     SEMINAR = 'SEM'
     SOCIAL = 'SOC'
     PARTY = 'PAR'
@@ -15,24 +26,21 @@ class Event(models.Model):
     MEETING = 'MTG'
     EVENT_CATEGORIES = [ (SEMINAR, 'Seminar'), (SOCIAL, 'Social'), (PARTY, 'Party'), (CONFERENCE, 'Conference'), (MEETING,'Meeting') ]
 
-    event_title = models.CharField(max_length=30)
-    description = models.CharField(max_length=300)
-    pub_date = models.DateTimeField(default=timezone.now)
+
     category = models.CharField(
         max_length=3,
         choices=EVENT_CATEGORIES,
         default=SOCIAL,
     )
 
+
     longitude = models.DecimalField(max_digits=5, decimal_places=2)
     latitude = models.DecimalField(max_digits=5, decimal_places=2)
 
-
-    rsvp = models.BooleanField(default=False)
-    private = models.BooleanField(default=False)
-
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    """
 
 
 
